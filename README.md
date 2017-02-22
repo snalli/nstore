85c19efc19dabbc5d265321020762e241c74ab21
Swapnil Haria <swapnilh@pratchett.cs.wisc.edu>
2017-02-22 16:27:12 -0600

    Added the run script

--- README.md
+++ README.md
@@ -10,7 +10,21 @@ Evaluation of different storage architectures in database systems designed for n
 - **g++ 4.7+** 
 - **autoconf** (`apt-get install autoconf libtool`) 
 
-## Setup
+
+## Easy install and run
+Use our install and run scripts to make your lives easier!
+
+We have provided a simple install.py which handles installation and cleans of
+the individual workloads. To clean and/or build nstore, simply do:
+
+    python install.py [--build] [--clean]
+
+For running either ycsb or tpcc workload with nstore,
+    run.py [-h] [--sim_size SIM_SIZE] [ycsb/tpcc]
+
+Supported simulation sizes: test, small, medium, large
+
+## Manual Setup
         
 ```
     ./bootstrap
---
+++ run.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import os
+from subprocess import Popen, PIPE
+
+sim_sizes = {'test':'./nstore -x100 -k1000 -e1 -w  -p0.5 -i', 
+            'small': './nstore -x1000 -k10000 -e2 -w -p0.5 -i -n',
+            'medium': './nstore -x20000 -k50000 -e2 -w -p0.5 -i -n ',
+            'large' : './nstore -x8000000 -k100000 -e4 -w -p0.8 -i -n'}
+
+def runCmd(cmd, err, out):
+    """
+    Takes two strings, command and error, runs it in the shell
+    and then if error string is found in stdout, exits.
+    For no output = no error, use err=""
+    """
+    print cmd
+    (stdout, stderr) = Popen(cmd, shell=True, stdout=PIPE).communicate()
+    if err is None:
+        if stdout != "":
+            print "Error: %s" %(out,)
+            print "Truncated stdout below:"
+            print '... ', stdout[-500:]
+            sys.exit(2)
+    else:
+        if err in stdout:
+            print "Error: %s" %(out,)
+            print "Truncated stdout below:"
+            print '... ', stdout[-500:]
+            sys.exit(2)
+
+def main(argv): 
+    """
+    Parses the arguments and cleans and/or builds the specified
+    workloads of the whisper suite
+    """
+    parser = argparse.ArgumentParser(description='Builds echo from the whisper suite.')
+    parser.add_argument('benchmarks', metavar='workload', type=str, nargs=1,
+                help='workloads to be run: tpcc/ycsb')
+    parser.add_argument('--sim_size', dest='sim_size', action='store', 
+            default='test', help='Simulation size: test, small, medium, large')
+
+    args = parser.parse_args()
+    print 'Running a ' + str(args.sim_size) + ' simulation for ' + str(args.benchmarks)
+    
+    if args.benchmarks == 'ycsb':
+        variant = ' -y'
+    else:
+        variant = ' -t'
+
+    os.chdir('src/')
+    if args.sim_size in sim_sizes:
+        runCmd(sim_sizes[args.sim_size] + variant, "Error", "Simulation failed")    
+    os.chdir('../')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
